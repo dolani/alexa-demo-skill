176578340ffc8ab0e4d0c33ab83824e25fce63a7
Ayo Akindolani <akindolaniayoola@gmail.com>
2022-10-10 17:15:36 +0100

    updated alexa messages

--- DemoPrayerSkill/Contract/AddPrayerIntentHandler.cs
+++ DemoPrayerSkill/Contract/AddPrayerIntentHandler.cs
@@ -18,13 +18,13 @@ namespace DemoPrayerSkill.Contract
             string msg = string.Empty;
             if (string.IsNullOrEmpty(itm))
             {
-                msg = "I didn't get that. Would you like to ask again?";
+                msg = "I did not get that. Would you like to ask again?";
                 Reprompt er = new Reprompt(msg);
                 var resp = ResponseBuilder.Ask(msg, er, session);
                 return Task.FromResult(resp);
             }
 
-            msg = $"What is your prayer request?";
+            msg = $"What is your prayer request, please?";
             Reprompt rp = new Reprompt(msg);
             var response = ResponseBuilder.Ask(msg, rp, session);
 
